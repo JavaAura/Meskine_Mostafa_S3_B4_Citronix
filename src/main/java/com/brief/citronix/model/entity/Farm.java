137fbc94ec3e599dcdc4b9d85e696f787363f842
MesVortex <meskinemostafa4@gmail.com>
2024-11-19 11:08:27 +0100

    CT-63 Configure application.properties file

--- src/main/java/com/brief/citronix/model/entity/Farm.java
+++ src/main/java/com/brief/citronix/model/entity/Farm.java
@@ -1,4 +1,26 @@
 package com.brief.citronix.model.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "farms")
 public class Farm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    private double area;
+
+    @Temporal(TemporalType.DATE)
+    private LocalDate creationDate;
+
+//    @OneToMany(mappedBy = "farm", cascade = CascadeType.ALL)
+//    private List<Field> fields = new ArrayList<>();
 }
+
